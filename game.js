bd75128ccb666a06c8259dec2addf1de43a896b9
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-05 20:03:18 -0500

    completed image displaying

--- game.js
+++ game.js
@@ -1,14 +1,58 @@
+const NUMTOIMAGE = {
+    1: "empty",
+    2: "c",
+    4: "cpp",
+    8: "php",
+    16: "rust",
+    32: "ruby",
+    64: "swift",
+    128: "sql",
+    256: "java",
+    512: "python",
+    1024: "ts",
+    2048: "js"
+};
+
 class Game {
     constructor() {
         // creates the board for play
-        this.board = [[0, 0, 0, 0],
-                      [0, 0, 0, 0],
-                      [0, 0, 0, 0],
-                      [0, 0, 0, 0]];
+        this.board = [[1, 1, 1, 1],
+                      [1, 1, 1, 1],
+                      [1, 1, 1, 1],
+                      [1, 1, 1, 1]];
     }
     
-    move() {
-        this.board[randInt(0, 3)][randInt(0, 3)] = 2;
+    move(method) {
+        this.board[randInt(0, 4)][randInt(0, 4)] = Math.pow(2, randInt(0, 12));
         console.log(this.board);
+    }
+
+    display(container) {
+
+        // clears the container
+        container.innerHTML = null;
+
+        // goes through every square on the board
+        for (let y = 0; y < 4; y++) {
+            for (let x = 0; x < 4; x++) {
+
+                // gets the image being looked at
+                let filename = NUMTOIMAGE[this.board[y][x]];
+
+                // gets the margins of the image relative to the rest of the board: img size: 100, margin: 10px
+                let top = y * (110); // distance from top
+                let left = x * (110); // distance from left
+                
+                // creates a new image with specified characteristics
+                let img = document.createElement("img");
+                img.setAttribute("src", "/imgs/" + filename + ".png");
+                img.setAttribute("class", "image");
+                img.style.top = String(top) + "px";
+                img.style.left = String(left) + "px";
+
+                // adds image to container
+                container.appendChild(img);
+            }
+        }
     }
 }
--- script.js
+++ script.js
@@ -1,19 +1,27 @@
+// creates a new game
 var game = new Game();
 
-document.addEventListener("keydown", (event) => {
-    var move = event.key;
-    var moves = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
-    var index = moves.findIndex((option) => {
-                    if (option === move) {
-                        return true;
-                    }
-                });
-    if (index === -1) {
-        return false;
-    }
-    else {
-        game.move(["up", "down", "left", "right"][index]);
-    }
+window.addEventListener("DOMContentLoaded", () => {
+
+    // gets the container in which the images are displayed in
+    const CONTAINER = document.getElementById("container");
+    console.log(CONTAINER);
+    
+    document.addEventListener("keydown", (event) => {
+        var moves = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+        var index = moves.findIndex((option) => {
+                        if (option === event.key) {
+                            return true;
+                        }
+                    });
+        if (index === -1) {
+            return false;
+        }
+        else {
+            game.move(["up", "down", "left", "right"][index]);
+            game.display(CONTAINER);
+        }
+    })
 });
 
 // returns a random number from the min (inclusive) to the max (exclusive)
